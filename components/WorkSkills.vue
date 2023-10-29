9b2882ca7e65dd89e1d6d22a344d8be8923e8a69
Jeff Paltera <hi@jeffp.one>
2023-10-29 20:57:26 +0800

    chore: remove 2 types of work skills

--- components/WorkSkills.vue
+++ components/WorkSkills.vue
@@ -1,25 +1,12 @@
 <template>
-    <div class="flex gap-5 flex-col items-start">
-        <div class="flex gap-2">
-            <div
-                v-for="tech of techStacks"
-                :key="tech.name"
-                class="flex flex-col items-center border p-5 rounded-xl w-[170px] text-center"
-            >
-                <Rating v-model="tech.value" readonly :cancel="false" />
-                {{ tech.name }}
-            </div>
-        </div>
-
-        <div class="flex gap-5">
-            <div
-                v-for="skill of skills"
-                :key="skill.name"
-                class="flex flex-col items-center border p-5 rounded-xl w-[170px] text-center"
-            >
-                <Rating v-model="skill.value" readonly :cancel="false" />
-                {{ skill.name }}
-            </div>
+    <div class="flex gap-2 flex-wrap justify-center">
+        <div
+            v-for="tech of techStacks"
+            :key="tech.name"
+            class="flex flex-col items-center border p-5 rounded-xl w-[170px] text-center"
+        >
+            <Rating v-model="tech.value" readonly :cancel="false" />
+            {{ tech.name }}
         </div>
     </div>
 </template>
@@ -32,9 +19,6 @@ const techStacks = ref([
     { value: 4, name: 'Node.JS' },
     { value: 3, name: 'Python' },
     { value: 3, name: 'PHP' },
-])
-
-const skills = ref([
     { value: 4, name: 'Vue/Nuxt.js 2&3' },
     { value: 4, name: 'React/Next.js' },
     { value: 4, name: 'Tailwind/UnoCSS' },
